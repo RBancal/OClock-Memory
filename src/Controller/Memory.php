250e659db3f5cfc17164c61fdeac2ea9b5060419
Raphaël BANCAL <raphael.bancal@orange.fr>
2021-04-05 17:17:50 +0200

    Début de mise en place de la structure du jeu

--- src/Controller/Memory.php
+++ src/Controller/Memory.php
@@ -29,10 +29,61 @@ class Memory
 
     public function game()
     {
+        if (!isset($_POST['difficulte'])) header("Location: ./index.php");
+        $level = $_POST['difficulte'];
+        $aMemory = array_fill(0, 4, []);
+
+        switch ($level) {
+            case 1:
+                $aCards = ['pomme', 'banane', 'orange', 'citron-vert', 'grenade', 'abricot', 'citron', 'fraise', 'pomme-verte', 'peche'];
+                $aMemory = $this->createArray($aMemory, 5);
+                $aMemory = $this->populateArray($aMemory, $aCards, 9);
+                break;
+            case 2:
+                $aCards = ['pomme', 'banane', 'orange', 'citron-vert', 'grenade', 'abricot', 'citron', 'fraise', 'pomme-verte', 'peche', 'raisin', 'pasteque', 'prune', 'poire'];
+                $aMemory = $this->createArray($aMemory, 7);
+                $aMemory = $this->populateArray($aMemory, $aCards, 13);
+                break;
+            case 3:
+                $aCards = ['pomme', 'banane', 'orange', 'citron-vert', 'grenade', 'abricot', 'citron', 'fraise', 'pomme-verte', 'peche', 'raisin', 'pasteque', 'prune', 'poire', 'cerise', 'framboise', 'mangue', 'cerise-jaune'];
+                $aMemory = $this->createArray($aMemory, 9);
+                $aMemory = $this->populateArray($aMemory, $aCards, 17);
+                break;
+        }
+
         $games = $this->gameDAO->getResults();
 
         return $this->view->render('game', [
             'games' => $games,
+            'aMemory' => $aMemory
         ]);
+    }
+
+    private function createArray(array $aMemory, int $nbrCases)
+    {
+        for ($i = 0; $i < 4; $i++) {
+            $aMemory[$i] = array_fill(0, $nbrCases, []);
+        }
+
+        return $aMemory;
+    }
+
+    private function populateArray(array $aMemory, array $aCards, int $indexLevel)
+    {
+        $index = 0;
+
+        $rows = sizeof($aMemory);
+        for ($i = 0; $i < $rows; $i++) {
+            $cols = sizeof($aMemory[$i]);
+            for ($j = 0; $j < $cols; $j++) {
+                $aMemory[$i][$j] = $aCards[$index];
+                $index < $indexLevel ? $index++ : $index = 0;
+            }
+            shuffle($aMemory[$i]);
+        }
+
+        shuffle($aMemory);
+
+        return $aMemory;
     }
 }
--- templates/game.php
+++ templates/game.php
@@ -1,17 +1,27 @@
 <?php $this->title = "Memory - Game" ?>
-<h1>Memory - Game</h1>
-<p>En construction</p>
-<?php
-foreach ($games as $game) {
-    ?>
-    <div>
-        <p><?= htmlspecialchars($game->getDate()); ?></p>
-        <p><?= htmlspecialchars($game->getUtilisateur()); ?></p>
-        <p><?= htmlspecialchars($game->getLevel()); ?></p>
-        <p><?= htmlspecialchars($game->getWin()); ?></p>
-        <p><?= htmlspecialchars($game->getTemps()); ?></p>
-    </div>
-    <br>
+<div id="informations">
+    <p>Utilisateur : <?php echo $_POST['utilisateur'] ?></p>
+    <p>Difficulté : <?php echo $_POST['difficulte'] ?></p>
+</div>
+<div id="game-board">
     <?php
-}
-?>
+    foreach ($aMemory as $row) {
+        ?>
+        <div class="game-row">
+            <?php
+            foreach ($row as $key => $value) {
+                ?>
+                <div class="fruit">
+                    <img class="<?php echo $value ?>" src="../public/img/cards.png" alt="">
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+        <?php
+    }
+    ?>
+</div>
+<div id="progress-bar">
+    <div id="progression"></div>
+</div>
